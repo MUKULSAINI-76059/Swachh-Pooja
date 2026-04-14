25fad94760fa6922d86771bfed9deb0da285110c
MUKUL <mukulsaini76059@gmail.com>
2026-04-15 01:49:17 +0530

    Update File

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: true,
+  secure: false,
   family: 4,
   auth: {
     user: process.env.EMAIL_USER,
